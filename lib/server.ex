577e583602b3e9c2946d8fe84cea2ed8d3528ee4
Ben Njoroge <ben.njoroge@bettyblocks.com>
2023-03-28 19:09:45 +0200

    change try a different option

--- lib/server.ex
+++ lib/server.ex
@@ -13,19 +13,15 @@ defmodule Server do
   Listen for incoming connections
   """
   def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     IO.puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    #
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, packet: :line, active: false, reuseaddr: true])
     server(socket)
   end
 
   defp server(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    :gen_tcp.send(client, :unicode.characters_to_binary("$4\r\nPONG\r\n"))
+    :gen_tcp.send(client, "$4\r\nPONG\r\n")
     :gen_tcp.shutdown(client, :read)
   end
-
 end
