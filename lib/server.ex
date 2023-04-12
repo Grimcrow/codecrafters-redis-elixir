cce7d5b4eef07aca453d8d39f55837845a606636
Ben Njoroge <ben.njoroge@bettyblocks.com>
2023-04-12 21:59:00 +0200

    Add support for multi connection and multiple pings per connection

--- lib/server.ex
+++ lib/server.ex
@@ -6,7 +6,12 @@ defmodule Server do
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
+    children = [
+      {Task.Supervisor, name: Request.TaskSupervisor},
+      {Task, fn -> Server.listen() end}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 
   @doc """
@@ -21,7 +26,33 @@ defmodule Server do
 
   defp server(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    :gen_tcp.send(client, "$4\r\nPONG\r\n")
-    :gen_tcp.shutdown(client, :read)
+    {:ok, pid} = Task.Supervisor.start_child(Request.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    server(socket)
   end
+
+  defp serve(client) do
+    client
+    |> read_data()
+    |> parse_data()
+    |> Enum.each(&write_data(client, &1))
+
+    serve(client)
+  end
+
+  defp read_data(client) do
+    {:ok, data} = :gen_tcp.recv(client, 0)
+    data
+  end
+
+  defp parse_data(data) do
+    data
+    |> String.trim()
+    |> String.split("\\n")
+  end
+
+  defp write_data(client, _data) do
+    :gen_tcp.send(client, "$4\r\nPONG\r\n")
+  end
+
 end
