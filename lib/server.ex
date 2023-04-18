bbd701647a7363d9ab37d32999c31b52750bcc86
Ben Njoroge <ben.njoroge@bettyblocks.com>
2023-04-18 22:39:32 +0200

    fix: only respond to ping requests

--- lib/server.ex
+++ lib/server.ex
@@ -23,6 +23,9 @@ defmodule Server do
 
   defp serve(client) do
     case read_data(client, "") do
+      {:ok, ""} ->
+        write_data(client, {:error, :closed})
+
       {:ok, command} ->
         command
         |> parse_data()
@@ -54,22 +57,22 @@ defmodule Server do
   defp parse_data(data) do
     data
     |> String.trim()
-    |> String.split("\\r\\n")
+    |> String.split("\r\n")
     |> parse_resp()
   end
 
-  defp parse_resp(["*" <> arr_count, _, _, _]) do
+  defp parse_resp(["*" <> arr_count, _, "ping"]) do
     arr_count = String.to_integer(arr_count)
     Enum.map(1..arr_count, fn _ -> "ping" end)
   end
 
-  defp parse_resp(data), do: data
+  defp parse_resp(_data), do: []
 
   defp write_data(_client, {:error, :closed}) do
     exit(:shutdown)
   end
 
   defp write_data(client, _data) do
-    :gen_tcp.send(client, :unicode.characters_to_binary("$4\r\nPONG\r\n"))
+    :gen_tcp.send(client, :unicode.characters_to_binary("+PONG\r\n"))
   end
 end
